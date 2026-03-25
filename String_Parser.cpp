d37133da78f9bacc804691226b8cf8172beb8115
aabdul1300 <abdulmananar2005@gmail.com>
2026-03-25 10:39:52 -0700

    Update String_Parser.cpp

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream> // For parsing lines
+#include <sstream> 
 using namespace std;
 
-void parseCSV(const string&);
+string getValue(string pair) {
+    size_t colonPos = pair.find(':');
+    if (colonPos != string::npos) {
+        return pair.substr(colonPos + 1);
+    }
+    return "";
+}
+
+void parseCSV(const string& filename) {
+    ifstream file(filename);
+    
+    if (!file.is_open()) {
+        cerr << "Error: Could not open file " << filename << endl;
+        return;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string namePair, agePair, initialPair, isStudentPair, gradePair;
+
+        if (ss >> namePair >> agePair >> initialPair >> isStudentPair >> gradePair) {
+            cout << "Name: " << getValue(namePair) << endl;
+            cout << "Age: " << getValue(agePair) << endl;
+            cout << "Initial: " << getValue(initialPair) << endl;
+            cout << "IsStudent: " << getValue(isStudentPair) << endl;
+            cout << "Grade: " << getValue(gradePair) << " ." << endl;
+            cout << "-----------------" << endl;
+        }
+    }
+
+    file.close();
+}
 
 int main() {
-   parseCSV("students.csv");
-
-   return 0;
+    parseCSV("students.csv");
+    return 0;
 }
